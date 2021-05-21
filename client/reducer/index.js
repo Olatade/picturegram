d960d977cff61c78a0c08a1f0c4bf6f0b39f8015
Olatade <4tade01@gmail.com>
2021-05-21 13:17:19 +0100

    Add redux reducers

---
+++ client/reducer/index.js
@@ -0,0 +1,13 @@
+// put all the reducers here
+
+import {combineReducers} from 'redux';
+import {routerReducer} from 'react-router-redux';
+
+import posts from './posts';
+import comments from './comments';
+
+
+// passing routing because we want to pass the changes in the url
+const rootReducer = combineReducers({posts, comments, router: routerReducer });
+
+export default rootReducer;
---
+++ client/reducer/posts.js
@@ -0,0 +1,16 @@
+// we need to create a reducer for each piece of state
+// all states can be found in client/store.js
+
+
+// a reducer takes in two things
+//1. the action
+//2. a copy of the current state
+
+
+// set state to empty
+function posts(state = [], action ){
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
