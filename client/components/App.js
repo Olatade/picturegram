9685a0f2b4fd80f8dd5a6489fc2d0b2c3938eb98
Olatade <4tade01@gmail.com>
2021-05-21 16:14:55 +0100

    Access dispatch and state in Main.js using redux

---
+++ client/components/App.js
@@ -0,0 +1,35 @@
+/*
+  This component will help
+  - inject data into the component where it is needed
+  - expose actions to buttons and components
+*/
+
+import { bindActionCreators} from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/actionCreators';
+import Main from './Main';
+
+
+
+// this function will do the magic and sureface our data
+function mapStateToProps(state){
+  return{
+    posts: state.posts,
+    Comments: state.comments
+  }
+}
+
+//this function will do the magic and surface our function
+function mapDispatchToProps(dispatch){
+  return bindActionCreators(actionCreators, dispatch);
+}
+
+
+/**
+ * mapStateToProps amd matchDispatchToProps will take our state (post and comments) and our dispatch ( the action creators)
+ * and surface those data in the functions via pros in our component
+ */
+// Take regular Main component and add (all of the data and props from stateToProps), (all of our action creators from dispatchToProps)
+const App = connect(mapStateToProps, matchDispatchToProps)(Main); 
+
+export default App;
