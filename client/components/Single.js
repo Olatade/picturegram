57548fea72d699d3f0fc9ac66a8da5109852bfdb
Olatade <4tade01@gmail.com>
2021-05-29 15:38:30 +0100

    Displaying and adding comments

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Photo from './photo';
-import Comment from './Comments'
+import Comments from './Comments'
 
 const Single = React.createClass({
   render() {
+    const { postId } = this.props.params;
     //index of the post
     // this.props.params.postId params is made available to us by the router. check picturegram "<Route path="/view/:postId" component={Single}></Route>"
     const i = this.props.posts.findIndex( (post) => post.code === this.props.params.postId ); // get the index of the post
     const post = this.props.posts[i];
-    console.log(post);
+    const postComments = this.props.comments[postId] || [];
     
     //get us the post
     return <div className="single-photo">
       <Photo i={i} post={post} {...this.props} />
+      <Comments postComments={postComments}/>
     </div>;
   },
 });
