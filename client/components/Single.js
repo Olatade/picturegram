bf3dd437ad8d8d9b8a99210ff8926e73ba8b21fd
Olatade <4tade01@gmail.com>
2021-05-29 14:18:00 +0100

    Display the single photo component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { Link } from "react-router";
+import Photo from './photo';
+import Comment from './Comments'
 
 const Single = React.createClass({
   render() {
-    return <div className="single-photo"> I am the single grid</div>;
+    //index of the post
+    // this.props.params.postId params is made available to us by the router. check picturegram "<Route path="/view/:postId" component={Single}></Route>"
+    const i = this.props.posts.findIndex( (post) => post.code === this.props.params.postId ); // get the index of the post
+    const post = this.props.posts[i];
+    console.log(post);
+    
+    //get us the post
+    return <div className="single-photo">
+      <Photo i={i} post={post} {...this.props} />
+    </div>;
   },
 });
 
---
+++ client/components/comments.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Comments = React.createClass({
+  render(){
+    return <div className="comment">i'm the comment</div>;
+  }
+});
+
+export default Comments;
